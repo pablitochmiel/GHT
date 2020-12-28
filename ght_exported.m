157b122665780ae627ab4bd278b87a898557510b
pablito_chmiel <pawcio27610@wp.pl>
2020-12-28 15:32:51 +0100

    add implementation of rotary ght from mathworks page

--- ght_exported.m
+++ ght_exported.m
@@ -12,6 +12,9 @@ classdef ght_exported < matlab.apps.AppBase
         EdgeDropDown       matlab.ui.control.DropDown
         sigmaSpinnerLabel  matlab.ui.control.Label
         sigmaSpinner       matlab.ui.control.Spinner
+        stepSpinnerLabel   matlab.ui.control.Label
+        stepSpinner        matlab.ui.control.Spinner
+        findrotateButton   matlab.ui.control.Button
     end
 
     
@@ -128,6 +131,196 @@ classdef ght_exported < matlab.apps.AppBase
             pause;
             %}
         end
+        
+        function [Ismarked,Iborders,Ybest,Xbest, ItmAng, BestScore]= MAIN_find_object_in_image(app,Is,Iedg,Itm)
+            %{
+            Find an object that fit Template Itm in image Is.
+            The orientation of the template and the object in the image does not have to be the same as that as the template. 
+            The template Itm is matched to the image Is in various of rotations and the best match is chosen. 
+            The function use  Generalized Hough transforms to match the template to the
+            image.
+            Input (Essential):
+            Is: Color image with the object to be found.
+            Itm: Template of the object to be found. The template is written as binary image with the boundary of the template marked 1(white) and all the rest of the pixels marked 0. 
+            Output
+            Ismarked: The image (Isresize) with the template marked upon it in the location of and size of the best match.
+            Iborders: Binary image of the borders of the template/object in its scale and located in the place of the best match on the image. 
+            Ybest Xbest: location on the image (in pixels) were the template were found to give the best score (location the top left pixel of the template Itm in the image).
+            ItmAng: The rotation angle of  the template in degrees that give the best match
+            BestScore: Score of the best match found in the scan (the score of the output).
+            Algorithm:
+            The function rotate the template Itm in various of angles and for each rotation search for the template in the image. 
+            The angle and location in the image that gave the best match for the template are chosen.
+            %}
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%initialize optiona paramters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            if (nargin<1)  
+                Is=imread('Is.jpg');  
+            end %Read image
+            if (nargin<2)  
+                Itm=imread('Itm.tif');
+            end %Read template image
+            %Is=rgb2gray(Is);
+            %Itm=logical(Itm);% make sure Itm is boolean image
+            BestScore=-100000;
+            close all;
+            imtool close all;
+            %%%%%%%%%%%%%%%%Some parameters%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%555555555
+    %         St=size(Itm);
+    %         Ss=size(Is);
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Main Scan  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            for Ang=1:app.stepSpinner.Value:360 % rotate the template Itm 1 degree at the time and look for it in the image Is
+                
+                disp([num2str((Ang)/3.6) '% Scanned']);
+              Itr=Rotate_binary_edge_image(app,Itm,Ang);
+            %----------------------------------------------------------------------------------------------------------------------------------------- 
+             % the actuall recogniton step of the resize template Itm in the orginal image Is and return location of best match and its score can occur in one of three modes given in search_mode
+                         [app.score,  app.y,app.x ]=Generalized_hough_transform(app,Is,Iedg,Itr);% use generalized hough transform to find the template in the image
+                 %--------------------------if the correct match score is better then previous best match write the paramter of the match as the new best match------------------------------------------------------
+                 if (app.score(1)>BestScore) % if item  result scored higher then the previous result
+                       BestScore=app.score(1);% remember best score
+                         Ybest=app.y(1);% mark best location y
+                       Xbest=app.x(1);% mark best location x
+                       ItmAng=Ang;
+                 end
+            %-------------------------------mark best found location on image----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------        
+            end
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%output%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%show   best match optional part can be removed %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+            if BestScore>-100000% Display best match
+                 Itr=Rotate_binary_edge_image(app,Itm,ItmAng);
+                        [yy,xx] =find(Itr);
+                         Ismarked=set2(app,Is,[yy,xx],255,Ybest,Xbest);%Mark best match on image
+                        imshow(Ismarked,'Parent',app.UIAxes);
+                        Iborders=false(size(Is));
+                   Iborders=set2(app,Iborders,[yy,xx],1,Ybest,Xbest);
+               
+            else % if no match 
+               disp('Error no match founded');
+                Ismarked=0;% assign arbitary value to avoid 
+                   %Iborders=0;
+                   Iborders=0;
+                
+            end
+        end
+        
+        function [mat]=Rotate_binary_edge_image(app,I,Ang)
+        %{
+        Rotate binary (logical) edge image (I) in (Ang) degrees 
+        The rotated output image will also also be a binary edge image.
+        The connectivity/topology of all edges/curves in the input image (I) will be maintained and the line thickness of the curves
+         in the output image (mat) will remain 1 pixel.
+        The center of rotation is the center of the image
+        The dimensions of the output image (mat) will be different from the 
+        input image (I) and will be set such that the rotated image is fully within the image frame.
+        Input
+        I: Binary  edge image (logical type) consist of lines and curves with 
+        thickness of one pixel (such as curves, contour line, template, or edge images)
+        Ang: Rotation angle of the image in Degrees
+         
+        Output
+        mat: Rotated version of the input image (I), also binary edge image, the connectivity/topology of the edges/curves in input image  (I)
+        is maintained and also the line thickness remain one pixel.
+        x' = x*cos(theta) - y*sin (theta); rotated coordinates
+        y' = x*sin(theta) + y*cos (theta);
+        %}
+        [Hight,Width]=size(I);
+        CntX=(Width+1)/2;
+        CntY=(Hight+1)/2; %find center of orignal image
+        theta=Ang/180*pi; % convert angle from degree  to radians
+        %============================find size of new image===========================================================================================
+        Rot=[   cos(theta) -sin(theta)
+                sin(theta)  cos(theta)];
+        Corners=[ 1-CntY     1-CntX
+                  1-CntY     Width-CntX
+                  Hight-CntY 1-CntX
+                  Hight-CntY Width-CntX];  %Corners of the old image  center at zero
+        NewCorn=Corners*Rot;   
+        NMaxX=max(NewCorn(:,2));% corners of new image
+        NMinX=min(NewCorn(:,2));
+        NMaxY=max(NewCorn(:,1));
+        NMinY=min(NewCorn(:,1));
+        NSizeY=NMaxY-NMinY; %Size of new image Y
+        NSizeX=NMaxX-NMinX; %Size of new image Y
+        Dx=-NMinX;          % Tanslation of new image x
+        Dy=-NMinY;          % Tanslation of new image y
+        mat = false(round(NSizeY+1),round(NSizeX+1));% create canvas for rotated image
+        %===============================================rotate image==================================================================================================
+        [app.y,app.x]=find(I);
+        [n,~]=size(app.x);
+        for f=1:n %scan all points on image
+             p1=[app.y(f)-CntY app.x(f)-CntX]*Rot+[Dy+1 Dx+1];
+             mat(round(p1(1)),round(p1(2)))=1;% mark rotated point
+            
+            
+            if app.x(f)>1 && I(app.y(f),app.x(f)-1)==1 % Connect horizontal neighbour 
+                   Nx=app.x(f)-1; Ny=app.y(f);
+                   p1=[app.y(f)-CntY, app.x(f)-CntX]*Rot+[Dy+1 Dx+1];
+                   p2=[Ny-CntY, Nx-CntX]*Rot+[Dy+1 Dx+1];
+                   mat=ConnectPoints(app,mat,round(p1(2)),round(p1(1)),round(p2(2)),round(p2(1)));
+            end
+            if app.y(f)>1 && I(app.y(f)-1,app.x(f))==1 %connect vertical neighbour
+                    Nx=app.x(f); Ny=app.y(f)-1;  
+                    p1=[app.y(f)-CntY, app.x(f)-CntX]*Rot+[Dy+1 Dx+1];
+                    p2=[Ny-CntY, Nx-CntX]*Rot+[Dy+1 Dx+1];
+                    mat=ConnectPoints(app,mat,round(p1(2)),round(p1(1)),round(p2(2)),round(p2(1)));
+            end
+            if app.y(f)>1  && app.x(f)>1 &&  I(app.y(f)-1,app.x(f))==0 && I(app.y(f),app.x(f)-1)==0 && I(app.y(f)-1,app.x(f)-1)==1 % connect diagonal neighbor
+                    Nx=app.x(f)-1; Ny=app.y(f)-1;  
+                    p1=[app.y(f)-CntY, app.x(f)-CntX]*Rot+[Dy+1 Dx+1];
+                    p2=[Ny-CntY, Nx-CntX]*Rot+[Dy+1 Dx+1];
+                    mat=ConnectPoints(app,mat,round(p1(2)),round(p1(1)),round(p2(2)),round(p2(1)));
+            end
+            if app.y(f)>1  && app.x(f)<Width &&  I(app.y(f)-1,app.x(f))==0 && I(app.y(f),app.x(f)+1)==0 && I(app.y(f)-1,app.x(f)+1)==1 % connect second diagonal neighbor
+                    Nx=app.x(f)+1; Ny=app.y(f)-1;  
+                    p1=[app.y(f)-CntY, app.x(f)-CntX]*Rot+[Dy+1 Dx+1];
+                    p2=[Ny-CntY, Nx-CntX]*Rot+[Dy+1 Dx+1];
+                    mat=ConnectPoints(app,mat,round(p1(2)),round(p1(1)),round(p2(2)),round(p2(1)));
+            end 
+            %}
+          
+        end
+        %imshow(mat);
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        function [mat]=ConnectPoints(~,mat,x1,y1,x2,y2)
+            % draw line between x1,y1 to x2,y2 on binary image mat
+            %make sure cordinates are inside image mat
+            SizeMat=size(mat);
+            x1=max(x1,1);x2=max(x2,1);y1=max(y1,1);y2=max(y2,1);
+            y1=min(y1,SizeMat(1));y2=min(y2,SizeMat(1));x1=min(x1,SizeMat(2));x2=min(x2,SizeMat(2));
+            if x1==x2 % for horizontal line
+               yy=linspace(y1,y2,abs(y1-y2)*5);
+               xx = ones(size(yy))*x1;
+            else % for none horizontal line
+               xx = linspace(x1,x2,abs(x1-x2)*5+abs(y1-y2)*5);   
+               yy = y1+(xx-x1)*(y2-y1)/(x2-x1);                     
+            end
+            round(yy);
+           index = sub2ind(size(mat),round(yy),round(xx));  
+            mat(index) = 1;   
+           %imshow(mat)
+           %pause(0.1);
+        end
+        
+        function [m2] = set2(~, m,k,v,y ,x )
+            % set value of  v in coordinates k of image m  with initial position x and y,
+            if nargin<4
+                y=0;
+                x=0;
+            end
+            if x<0
+                x=0;
+            end
+            if y<0
+                y=0;
+            end
+            [a,~]=size(k);
+            m2=m;
+            for f=1:1:a
+                m2(k(f,1)+y,k(f,2)+x)=v;
+            %m(6,6)=7;
+            end
+        end
     end
     
 
@@ -137,6 +330,7 @@ classdef ght_exported < matlab.apps.AppBase
         % Code that executes after component creation
         function startupFcn(app)
             app.findButton.Enable='off';
+            app.findrotateButton.Enable='off';
             app.UIAxes.Visible='off';
             app.sigmaSpinner.Enable='off';
         end
@@ -160,6 +354,7 @@ classdef ght_exported < matlab.apps.AppBase
                 %app.Image.ImageSource=app.image;
                 if ~isempty(app.shape)
                     app.findButton.Enable='on';
+                    app.findrotateButton.Enable='on';
                 end
                 app.Label.Text="image added";
             end
@@ -176,6 +371,7 @@ classdef ght_exported < matlab.apps.AppBase
                 %imtool(app.shape);
                 if ~isempty(app.imageGray)
                     app.findButton.Enable='on';
+                    app.findrotateButton.Enable='on';
                 end
                 app.Label.Text="shape added";
              end
@@ -201,7 +397,7 @@ classdef ght_exported < matlab.apps.AppBase
                 end
             end
             app.Label.Text="found "+string(s)+" shapes, score "+string(app.score(1));
-            imshow(temp,'parent',app.UIAxes);
+            imshow(temp,'Parent',app.UIAxes);
         end
 
         % Value changed function: EdgeDropDown
@@ -240,6 +436,29 @@ classdef ght_exported < matlab.apps.AppBase
             imshow(app.imageEdg,'parent',app.UIAxes);
             app.Label.Text="edges found";
         end
+
+        % Button pushed function: findrotateButton
+        function findrotateButtonPushed(app, event)
+            app.Label.Text="shape search in progress";
+            [~,~,app.y,app.x,kat,app.score]=MAIN_find_object_in_image(app,app.imageGray,app.imageEdg,app.shape);
+            s=size(app.x,1);
+%             [a,b,~]=size(app.image);
+%             temp=zeros([a,b,3],'uint8');
+%             temp(:,:,:)=app.image(:,:,:);
+%             for k=1:s
+%                 for i=1:size(app.shape,1)
+%                     for j=1:size(app.shape,2)
+%                         if (app.shape(i,j))
+%                             temp(i+app.y(k),j+app.x(k),1)=255;
+%                             temp(i+app.y(k),j+app.x(k),2)=0;
+%                             temp(i+app.y(k),j+app.x(k),3)=0;
+%                         end
+%                     end
+%                 end
+%             end
+            app.Label.Text="found "+string(s)+" shapes, score "+string(app.score(1))+', kat '+string(kat);
+%            imshow(temp,'Parent',app.UIAxes);
+        end
     end
 
     % Component initialization
@@ -250,30 +469,30 @@ classdef ght_exported < matlab.apps.AppBase
 
             % Create UIFigure and hide until all components are created
             app.UIFigure = uifigure('Visible', 'off');
-            app.UIFigure.Position = [100 100 640 480];
+            app.UIFigure.Position = [100 100 826 530];
             app.UIFigure.Name = 'MATLAB App';
 
             % Create imageButton
             app.imageButton = uibutton(app.UIFigure, 'push');
             app.imageButton.ButtonPushedFcn = createCallbackFcn(app, @imageButtonPushed, true);
-            app.imageButton.Position = [18 388 100 22];
+            app.imageButton.Position = [18 480 100 22];
             app.imageButton.Text = 'image';
 
             % Create shapeButton
             app.shapeButton = uibutton(app.UIFigure, 'push');
             app.shapeButton.ButtonPushedFcn = createCallbackFcn(app, @shapeButtonPushed, true);
-            app.shapeButton.Position = [407 388 100 22];
+            app.shapeButton.Position = [391 480 100 22];
             app.shapeButton.Text = 'shape';
 
             % Create findButton
             app.findButton = uibutton(app.UIFigure, 'push');
             app.findButton.ButtonPushedFcn = createCallbackFcn(app, @findButtonPushed, true);
-            app.findButton.Position = [517 388 100 22];
+            app.findButton.Position = [498 480 100 22];
             app.findButton.Text = 'find';
 
             % Create Label
             app.Label = uilabel(app.UIFigure);
-            app.Label.Position = [42 64 235 22];
+            app.Label.Position = [42 37 235 22];
             app.Label.Text = '';
 
             % Create UIAxes
@@ -281,25 +500,25 @@ classdef ght_exported < matlab.apps.AppBase
             title(app.UIAxes, '')
             xlabel(app.UIAxes, '')
             ylabel(app.UIAxes, '')
-            app.UIAxes.Position = [1 85 640 291];
+            app.UIAxes.Position = [1 82 826 373];
 
             % Create EdgeDropDownLabel
             app.EdgeDropDownLabel = uilabel(app.UIFigure);
             app.EdgeDropDownLabel.HorizontalAlignment = 'right';
-            app.EdgeDropDownLabel.Position = [128 388 34 22];
+            app.EdgeDropDownLabel.Position = [117 480 34 22];
             app.EdgeDropDownLabel.Text = 'Edge';
 
             % Create EdgeDropDown
             app.EdgeDropDown = uidropdown(app.UIFigure);
             app.EdgeDropDown.Items = {'Sobel', 'Prewitt', 'Roberts', 'log', 'Canny'};
             app.EdgeDropDown.ValueChangedFcn = createCallbackFcn(app, @EdgeDropDownValueChanged, true);
-            app.EdgeDropDown.Position = [177 388 100 22];
+            app.EdgeDropDown.Position = [166 480 100 22];
             app.EdgeDropDown.Value = 'Sobel';
 
             % Create sigmaSpinnerLabel
             app.sigmaSpinnerLabel = uilabel(app.UIFigure);
             app.sigmaSpinnerLabel.HorizontalAlignment = 'right';
-            app.sigmaSpinnerLabel.Position = [289 388 33 22];
+            app.sigmaSpinnerLabel.Position = [276 480 33 22];
             app.sigmaSpinnerLabel.Text = 'sigma';
 
             % Create sigmaSpinner
@@ -307,8 +526,26 @@ classdef ght_exported < matlab.apps.AppBase
             app.sigmaSpinner.Step = 0.1;
             app.sigmaSpinner.Limits = [0.1 Inf];
             app.sigmaSpinner.ValueChangedFcn = createCallbackFcn(app, @sigmaSpinnerValueChanged, true);
-            app.sigmaSpinner.Position = [334 388 63 22];
+            app.sigmaSpinner.Position = [321 480 63 22];
             app.sigmaSpinner.Value = 2;
+
+            % Create stepSpinnerLabel
+            app.stepSpinnerLabel = uilabel(app.UIFigure);
+            app.stepSpinnerLabel.HorizontalAlignment = 'right';
+            app.stepSpinnerLabel.Position = [605 480 28 22];
+            app.stepSpinnerLabel.Text = 'step';
+
+            % Create stepSpinner
+            app.stepSpinner = uispinner(app.UIFigure);
+            app.stepSpinner.Limits = [1 360];
+            app.stepSpinner.Position = [648 480 63 22];
+            app.stepSpinner.Value = 2;
+
+            % Create findrotateButton
+            app.findrotateButton = uibutton(app.UIFigure, 'push');
+            app.findrotateButton.ButtonPushedFcn = createCallbackFcn(app, @findrotateButtonPushed, true);
+            app.findrotateButton.Position = [716 480 100 22];
+            app.findrotateButton.Text = 'find rotate';
 
             % Show the figure after all components are created
             app.UIFigure.Visible = 'on';
