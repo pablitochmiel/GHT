72fd8616e0638bdb50432494044705f067ece24b
pablito_chmiel <pawcio27610@wp.pl>
2021-01-07 19:28:52 +0100

    add display to single search

--- korelacja_do_porownania_z_GHT.m
+++ korelacja_do_porownania_z_GHT.m
@@ -1,17 +1,36 @@
 close all; clear; clc;
 
-image=imread('shape_moj.png');
-image=rgb2gray(image);
-
+image_rgb=imread('shape_moj.png');
+image=rgb2gray(image_rgb);
+%test
 imageEdg=edge(image);         %nie wywala
 %imshow(imageEdg);
-shape=imread('tr.png');
+shape=imread('eksperyment.png');
 
 shape=(shape(:,:,1)==0);
 
+tic;
 res=filter2(shape,imageEdg);
+time=toc;
 res=res./max(max(res));
 [y,x]=find(res==1);
-
-imtool(res)
+s=size(x,1);
+[a,b,~]=size(image);
+[c,d]=size(shape);
+temp=zeros([a,b,3],'uint8');
+temp(:,:,:)=image_rgb(:,:,:);
+for k=1:s
+    for i=1:c
+        for j=1:d
+            if (shape(i,j))
+                temp(i+y(k)-round(0.5*c),j+x(k)-round(0.5*d),1)=255;
+                temp(i+y(k)-round(0.5*c),j+x(k)-round(0.5*d),2)=0;
+                temp(i+y(k)-round(0.5*c),j+x(k)-round(0.5*d),3)=0;
+            end
+        end
+    end
+end
+Text="found "+string(s)+' shapes, time: '+string(time)
+imshow(temp);
+%imtool(res)
  
